aa3bac7f8a75b9b8ef535c65dcaa948fdc3a2db5
Phoenix <x.rhythm@qq.com>
2024-09-02 19:15:48 +0800

    升级 SPM demo的 RheaTime 版本号

--- SPMExample/RheaExtension/Package.swift
+++ SPMExample/RheaExtension/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "RheaExtension", targets: ["RheaExtension"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/reers/Rhea.git", from: "1.0.0")
+        .package(url: "https://github.com/reers/Rhea.git", from: "1.0.1")
     ],
     targets: [
         .target(
