28c7126c71d12410e1f59572313e3622e639114f
Phoenix <x.rhythm@qq.com>
2024-10-09 14:56:48 +0800

    update example for spm

--- SPMExample/RheaExtension/Package.swift
+++ SPMExample/RheaExtension/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "RheaExtension", targets: ["RheaExtension"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/reers/Rhea.git", from: "1.0.5")
+        .package(url: "https://github.com/reers/Rhea.git", from: "1.0.6")
     ],
     targets: [
         .target(
